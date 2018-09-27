d3a1ffa07257d67dc19d2d659018d12efac6f50d
Roscoe van Wyk <roscoew@afrihost.com>
2018-09-27 13:47:11 +0200

    my first commit

---
+++ src/Profiles/RossvanWykProfile.php
@@ -0,0 +1,21 @@
+<?php
+namespace mostertb\PHPSA2018Profiles\Profiles;
+
+class RossvanWykProfile extends AbstractProfile
+{
+    public function getName()
+    {
+        return 'Ross van Wyk';
+    }
+
+    public function getBiography()
+    {
+        return 'Ross also just wants a sticker... :P';
+    }
+
+    public function getTwitterUsername()
+    {
+        return '@owkkuri';
+    }
+
+}
