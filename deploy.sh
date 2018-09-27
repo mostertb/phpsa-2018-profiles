70068e172ad190c34318f516534c8acb6e861283
Brad Mostert <mostertb@users.noreply.github.com>
2018-09-27 13:44:27 +0200

    Add priduction deploy script

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# This deploy script is run regularly in cron on the production server
+
+# reset to consistent state
+git reset --hard
+git checkout master
+
+# Pull the latest things from Github
+git pull
+
+# Composer install
+composer install --no-dev --optimize-autoloader --no-interaction
