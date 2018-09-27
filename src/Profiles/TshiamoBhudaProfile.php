6ba6fb91581d824c671b4b1e1b5efe5f0bb32e06
Tshiamo <tshiamobhuda@gmail.com>
2018-09-27 14:30:45 +0200

    Added my profile Code -- Tshiamo Bhuda - implemented the required methods

---
+++ src/Profiles/TshiamoBhudaProfile.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace mostertb\PHPSA2018Profiles\Profiles;
+
+class TshiamoBhudaProfile extends AbstractProfile
+{
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'Tshiamo Bhuda';
+    }
+
+    /**
+     * @return string
+     */
+    public function getBiography()
+    {
+        return 'Tshiamo Bhuda is Junior Developer at MiWay. <br>'
+            .'He is a fun person who likes solving problems and writing code. <br>'
+            .'His days are usually filled with excessive coffee drinking and coding. <br>'
+            .'He also loves music and tranquil places & enjoys sharing his skills to anyone willing to learn.';
+    }
+
+    /**
+     * @inheritdoc
+     * @return string
+     */
+    public function getGitHubUsername()
+    {
+        return 'tshiamobhuda';
+    }
+
+    /**
+     * @return string
+     */
+    public function getTwitterUsername()
+    {
+        return 'tsitsideepbhuda';
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getProfileImageURL()
+    {
+        return 'https://s.gravatar.com/avatar/dd1ae5845026c61e6669b5a01aff2b54?s=80';
+    }
+
+    /**
+     * @return array
+     */
+    public function getMaintainedProjects()
+    {
+        return ['tshiamobhuda/brickface' => 'https://github.com/tshiamobhuda/brickface'];
+    }
+
+
+}
