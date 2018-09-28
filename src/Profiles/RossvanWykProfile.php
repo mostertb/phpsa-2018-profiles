99520028f91bd6d9a2d0e134907d823f16c724b8
Roscoe van Wyk <roscoew@afrihost.com>
2018-09-28 11:28:48 +0200

    add docblocks and image url

--- src/Profiles/RossvanWykProfile.php
+++ src/Profiles/RossvanWykProfile.php
@@ -1,27 +1,52 @@
 <?php
 namespace mostertb\PHPSA2018Profiles\Profiles;
 
+/**
+ * Class RossvanWykProfile
+ * @package mostertb\PHPSA2018Profiles\Profiles
+ */
 class RossvanWykProfile extends AbstractProfile
 {
+    /**
+     * @return string
+     */
     public function getName()
     {
         return 'Ross van Wyk';
     }
 
+    /**
+     * @return string
+     */
     public function getBiography()
     {
         return 'Ross also just wants a sticker... :P';
     }
 
+    /**
+     * @return string
+     */
     public function getGitHubUsername()
     {
         return 'Owkkuri';
     }
 
 
+    /**
+     * @return string
+     */
     public function getTwitterUsername()
     {
         return '@owkkuri';
     }
+
+    /**
+     * @return null|string
+     */
+    public function getProfileImageURL()
+    {
+        return 'https://avatars1.githubusercontent.com/u/234763';
+    }
+
 
 }
